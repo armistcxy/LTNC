1a962b77146315b42be973a09a77664e7488bfb8
Hoang Le Ngo <adlehoang118@gmail.com>
2023-02-18 09:41:45 +0700

    fix problem
    clear

--- Week2/C/C9.cpp
+++ Week2/C/C9.cpp
@@ -1,13 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-	int n; cin >> n;
-	int mx = INT_MAX, my = INT_MAX;
-	for (int i = 1; i <= n; i++)
-	{
-		int a,b; cin >> a >> b;
-		mx = min(mx,a), my = min(my,b);
-	}
-	cout << mx * my;
+#define ll long long
+#define all(x) x.begin(),x.end()
+#define rep(i,a,b) for(int i=a;i<=b;i++)
+
+const ll INF = 1e16 + 5;
+void solve(){
+    int n; cin >> n;
+    unsigned long long mx = INF, my = INF;
+    while (n--)
+    {
+        unsigned long long a, b; cin >> a >> b;
+        mx = min(mx,a), my = min(my,b);
+    }
+    cout << mx * my << '\n';
+}
+int main(){
+#ifndef ONLINE_JUDGE
+  freopen("input.txt","r",stdin);
+  freopen("ouput.txt","w",stdout);
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    while (t--){
+        solve();
+    }
 }
